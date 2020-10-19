830c0f43f1d45e1c64736f3f20c48979d29e295c
venkatsunilm <minchala.venkatsunil@rampgroup.com>
2020-10-19 19:12:01 +0530

    Sample application code

--- app/src/main/java/com/example/myapplication/GlobalConstants.java
+++ app/src/main/java/com/example/myapplication/GlobalConstants.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 public class GlobalConstants {
 
     public static final String LOGTAG = "MFC";
-    public static final boolean IS_HARDWARE = false;
-    public static final boolean IS_ANDROID_PHONE = false;
+    public static final boolean IS_HARDWARE = true;
+    public static final boolean IS_ANDROID_PHONE = true;
 
 }
